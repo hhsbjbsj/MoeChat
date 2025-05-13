c4cdb19a0b18dd20ae606664a637256445227952
AlfreScarlet <659186445@qq.com>
2025-05-13 17:09:29 +0800

    删除测试文件

--- test.py
+++
@@ -1,7 +0,0 @@
-import re
- 
-text = "这是[example1]测试[example2]字符串[example3]"
-# matches = re.findall(r'\[(.*?)\]', text)
-matches = re.findall(r'\[(.*?)\]', text)
-print(matches)  # 输出: ['example1', 'example2', 'example3']
-
